7d1c46cd6e5e44012bcdc1ad723407aaab2ad408
Adrian Buchnat <adrianbuchnat@gmail.com>
2024-02-24 23:51:16 +0100

    Creating file to create db and tables

--- create_db.py
+++ create_db.py
@@ -1,41 +1,53 @@
-import psycopg2
-from psycopg2 import sql
+from psycopg2 import connect, OperationalError
+from psycopg2.errors import DuplicateDatabase, DuplicateTable
 
-database_params = {
-    'host': '127.0.0.1',
-    'user': 'postgres',
-    'password': 'admin12345',
-}
+CREATE_DB = "CREATE DATABASE databaseintegration;"
+
+CREATE_USERS_TABLE = """CREATE TABLE users(
+    id serial PRIMARY KEY, 
+    username varchar(255) UNIQUE,
+    hashed_password varchar(80))"""
+
+CREATE_MESSAGES_TABLE = """CREATE TABLE messages(
+    id SERIAL, 
+    from_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    to_id INTEGER REFERENCES users(id) ON DELETE CASCADE, 
+    text varchar(255),
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"""
+
+DB_USER = "postgres"
+DB_PASSWORD = "admin12345"
+DB_HOST = "127.0.0.1"
 
 try:
-    # connection to the postgresSQL database
-    connection = psycopg2.connect(**database_params)
+    cnx = connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+    try:
+        cursor.execute(CREATE_DB)
+        print("Database created")
+    except DuplicateDatabase as e:
+        print("Database exists ", e)
+    cnx.close()
+except OperationalError as e:
+    print("Connection Error: ", e)
 
-    #Cursor object to execute SQL queries
-    cursor = connection.cursor()
+try:
+    cnx = connect(database="databaseintegration", user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+    cnx.autocommit = True
+    cursor = cnx.cursor()
 
-    #check if database alredy exist
-    cursor.execute(
-        sql.SQL("SELECT 1 FROM pg_database WHERE datname = %s;"), ["databaseForProject"]
-    )
+    try:
+        cursor.execute(CREATE_USERS_TABLE)
+        print("Table users created")
+    except DuplicateTable as e:
+        print("Table exists ", e)
 
-    if not cursor.fetchone():
-        # If database do not exist create it
-        cursor.execute(
-            sql.SQL("CREATE DATABASE databaseForProject;")
-            )
-
-        print("Database created successfully")
-
-    else:
-        print("Database alredy exist")
-
-except (Exception, psycopg2.Error) as error:
-    print("Error connecting to PostgresSQL database: ", error)
-
-finally:
-    # Close the database connection and cursor;
-    if connection:
-        cursor.close()
-        connection.close()
-        print("PostgresSQL connection is closed.")
+    try:
+        cursor.execute(CREATE_MESSAGES_TABLE)
+        print("Table messages created")
+    except DuplicateTable as e:
+        print("Table exists ", e)
+    cnx.close()
+except OperationalError as e:
+    print("Connection Error: ", e)
